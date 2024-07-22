12e227a7f5cef30ba7c3c6e264866e3d48af69ee
ergemp <ergemp@gmail.com>
2024-07-22 19:19:19 +0300

    adding examples for datatypes

---
+++ src/main/java/org/ergemp/MatLibExamples/PowExample.java
@@ -0,0 +1,13 @@
+package org.ergemp.MatLibExamples;
+
+import static java.lang.Math.pow;
+
+public class PowExample {
+    public static void main(String[] args) {
+
+        float number = 4.321f;
+        System.out.println(number);
+
+        System.out.println(pow(number,2.0));
+    }
+}
---
+++ src/main/java/org/ergemp/dataTypes/CharTypeExamples.java
@@ -0,0 +1,10 @@
+package org.ergemp.dataTypes;
+
+public class CharTypeExamples {
+    public static void main(String[] args) {
+
+        char char1 = 'H';
+
+
+    }
+}
