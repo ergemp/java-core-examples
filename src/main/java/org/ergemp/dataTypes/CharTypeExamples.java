fd6af73fcfcbecdd67fe46c99f9d7a7909a26d93
ergemp <ergemp@gmail.com>
2024-07-22 19:37:31 +0300

    .gitignore added

--- src/main/java/org/ergemp/dataTypes/CharTypeExamples.java
+++ src/main/java/org/ergemp/dataTypes/CharTypeExamples.java
@@ -4,6 +4,16 @@ public class CharTypeExamples {
     public static void main(String[] args) {
 
         char char1 = 'H';
+        char char2 = 'e';
+        char char3 = 'l';
+        char char4 = 'l';
+        char char5 = 'o';
+
+        System.out.print(char1);
+        System.out.print(char2);
+        System.out.print(char3);
+        System.out.print(char4);
+        System.out.print(char5);
 
 
     }
